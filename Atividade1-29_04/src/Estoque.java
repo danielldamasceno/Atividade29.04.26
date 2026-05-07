36975a4d9855b5fbb9226f4a1afcc9922e0aee36
danielldamasceno <daniel.damasceno@undf.edu.br>
2026-05-06 23:11:44 -0300

    Atividade1

---
+++ Atividade1-29_04/src/Estoque.java
@@ -0,0 +1,21 @@
+
+public class Estoque {
+	
+	String nomeProduto;
+	double preço;
+	int quantidade;
+	
+	public void quantidadeEstoque() {
+		System.out.println("Produto: "+ nomeProduto + "\n Preço: "+ preço + "\n Quantidade em estoque: "+ quantidade);
+	}
+	
+	public void adicionarEstoque() {
+		System.out.println("Escolha o produto")
+	}
+	
+	public void retirarEstoque() {
+		
+	}
+		
+		
+}
